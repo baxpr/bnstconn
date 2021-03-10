db54e8636c1312a424937c3c2272aedf42f7d7e2
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-03-10 12:22:05 -0600

    ACC rois

--- test_sing.sh
+++ test_sing.sh
@@ -4,12 +4,14 @@ singularity run --contain --cleanenv \
 --home $(pwd)/INPUTS \
 --bind INPUTS:/INPUTS \
 --bind OUTPUTS:/OUTPUTS \
-baxpr-mniconn-master-v2.0.3.simg \
-wroi_niigz ABHHIP_LR.nii.gz \
+baxpr-mniconn-master-v3.0.0.simg \
+wroi_niigz /INPUTS/rois_PMAT.nii.gz \
+wroilabel_csv rois_PMAT-labels.csv \
 wremovegm_niigz /INPUTS/filtered_removegm_noscrub_wadfmri.nii.gz \
 wkeepgm_niigz /INPUTS/filtered_keepgm_noscrub_wadfmri.nii.gz \
 wmeanfmri_niigz /INPUTS/wmeanadfmri.nii.gz \
 wbrainmask_niigz /INPUTS/rwmask.nii.gz \
 wt1_niigz /INPUTS/wmt1.nii.gz \
+connmaps_out no \
 out_dir /OUTPUTS
 
